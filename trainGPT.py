f5b5c549501afd35489b4845bb5006e03555184d
Your Name <anant2025grad@gmail.com>
2026-04-16 19:38:19 -0400

    refactor + cleanup: restructure bigram model with batching, eval loop, and text generation. removed unwanted print statements and added more contextually helpful comments.

--- trainGPT.py
+++ trainGPT.py
@@ -1,14 +1,24 @@
-import numpy
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+#hyperparameters
+batch_size = 32 # how many independent sequences will we process in parallel?
+block_size = 8 # what is the maximum context length for predictions?
+max_iters = 3000
+eval_interval = 300
+learning_rate = 1e-2
+eval_iters = 200
+
+torch.manual_seed(1337)
+
 # read 'input.txt' file
 with open('input.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # print length of text in 'input.txt' file
+print('')
 print("Length of text in document: ", len(text))
-
-# print first 1000 characters in the file
-print(text[:1000])
-
 # created sorted list of all unique characters in this set (first 1000 characters)
 chars = sorted(list(set(text)))
 # All unique characters used in text set
@@ -18,44 +28,19 @@ print("All vocab in set:")
 print(''.join(chars))
 print()
 
+
 # create a tokenization protocol/mapping for characters to integer
 strToInt = {char:int for int, char in enumerate(chars)}
 intToStr = {int:char for int, char in enumerate(chars)}
 encode = lambda str: [strToInt[c] for c in str] # encoding mechanism: takes a string, outputs a list of integers
 decode = lambda int: ''.join([intToStr[i] for i in int]) # decoding mechanism: takes a list of integers, outputs a string
 
-print(encode("hello"))
-print(decode(encode("hello")))
 
-# encode the entire text dataset using pyTorch and store it into a torch.Tensor
-import torch
+# encode the entire text dataset using pyTorch and store it into a tensor
 data = torch.tensor(encode(text), dtype = torch.long)
-print(data.shape, data.type)
-print(data[:1000]) # the 1000 character set we looked at earlier will be read by the GPT like this
-
-# split the data into training data and validation data
-split = int(0.9*len(data)) # split data 90/10
-# first 90% is used for training, other 10% is used for validation
+split = int(0.9*len(data)) # split the data into training data and validation data 90/10, first 90% is used for training, other 10% is used for validation
 training_data = data[:split]
 validation_data = data[split:]
-
-#block size is the number of previous tokens that will be taken into consideration when making a prediction
-block_size = 8
-var = training_data[:block_size + 1]
-
-# creates context-target pairs that train the model efficiently
-x = training_data[:block_size]
-y = training_data[1:block_size+1]
-for t in range(block_size):
-    context = x[:t+1]
-    target = y[t]
-    print(f"when input is {context} the target is {target}")
-
-torch.manual_seed(1337)
-batch_size = 4 #number of independent sequences processed in parallel
-block_size = 8 # maximum context length for predictions
-
-
 
 def getBatch(split):
     #decide whether the data is used for training or validation
@@ -66,33 +51,22 @@ def getBatch(split):
     y = torch.stack([data[i+1:i+block_size+1] for i in ix])
     return x, y
 
-xb, yb = getBatch('train')
-print('inputs: ')
-print(xb.shape)
-print(xb)
-print('targets: ')
-print(yb.shape)
-print(yb)
 
-print('-----')
-
-for batch in range(batch_size):
-    for time in range(block_size):
-            #what the model reads
-            context = xb[batch, :time+1]
-            #what the model predicts
-            target = yb[batch, time]
-            print(f"when input is {context.tolist()} the target is {target}")
-
+@torch.no_grad() #context manager that is useful if '.backward()' is not going to be used; good for memory efficiency
+def estimate_loss():
+    out = {}
+    m.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = getBatch(split)
+            logits, loss = m(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    m.train()
+    return out
 
 # pytorch module that implements the bigram language model
-
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-torch.manual_seed(1337)
-
-
 class BigramLanguageModel(nn.Module):
     def __init__(self, vocab_size):
         super().__init__()
@@ -132,7 +106,25 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 m = BigramLanguageModel(vocab_size)
-logits, loss = m(xb, yb)
-print(logits.shape)
-print(loss)
-print(decode(m.generate(idx = torch.zeros((1, 1), dtype = torch.long), max_new_tokens = 100) [0].tolist()))
+
+# create a pyTorch optimizer by implementing AdamW
+optimizer = torch.optim.Adam(m.parameters(), lr = 1e-3)
+
+for step in range(max_iters):
+
+    if step % eval_interval == 0:
+        loss = estimate_loss()
+        print(f"step {step}: train loss {loss['train']:.4f}, val loss {loss['val']:.4f} ")
+
+    # sample of a bunch of data
+    input, expected_output = getBatch('train')
+
+    # evaluate the loss
+    logits, loss = m(input, expected_output)
+    # zero out all the gradients
+    optimizer.zero_grad(set_to_none = True)
+    loss.backward()
+    optimizer.step()
+
+context = torch.zeros((1, 1), dtype = torch.long)
+print(decode(m.generate(context, max_new_tokens = 500) [0].tolist()))
